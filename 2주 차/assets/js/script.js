1facbc838d83dde1b09f4d7b3e1d58ee007171b6
정예빈 <144046262+yebin-jeong@users.noreply.github.com>
2024-07-23 15:46:37 +0900

    Delete 2주 차 directory

--- 2주 차/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const boardElement = document.getElementById('board');
-
-for(let i=0; i<10; i++){
-    for(let j=0; j<15; j++){
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        boardElement.appendChild(cell);
-    }
-}    
